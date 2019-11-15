4a2bf96a9a445cf51109eaaab51b6e8e6818362e
SEBASTIAN LUBERRIAGA <sebastian.luberriaga@mercadolibre.com>
2019-11-15 15:19:10 -0300

    update juan claudio ari gaston

--- lib/config.go
+++ lib/config.go
@@ -30,6 +30,10 @@ func GetConfig() Config {
 		HandleMessageTelegramURL: os.Getenv("HANDLE_MESSAGE_PUBLIC_URL"),
 		MaxTimesToNotify:         2,
 		UserDirectory: map[TelegramUserID]JiraUser{
+			465904347: {"5a6735f3a79cc4281ee3e6bd", "Juan"},
+			597295726: {"5d10ca8328a0740c8e0b2c79", "Gastón"},
+			551386294: {"5a9eb55f7cbc742a5c1e0863", "Ariel"},
+			151575607: {"5c6c717736d4d54c4112f27f", "Claudio"},
 			740387286: {"5bd85c501582cc3b70157386", "Seba"},
 		},
 		BugDeadline: time.Hour * 24 * 7,
